b0f7459c1cf6588cbbe2e65a44b63a9e0a6d5bc3
Gus Segura <kyanyoga@yahoo.com>
2015-11-09 14:17:00 -0600

    Create rediscreds.js

---
+++ rediscreds.js
@@ -0,0 +1,9 @@
+// redis credentials for server
+var rediscreds = {
+ REDIS_URL:'blahblah.redis.cache.windows.net',
+ REDIS_PORT: 6379,
+ REDIS_PRIM:'',
+ REDIS_SECD:''
+};
+
+module.exports = rediscreds;
